2024648065b8c5aa0e9fa81b9116289db1bacca4
Shintaro Abe <dictav@gmail.com>
2024-06-18 14:57:53 +0900

    feat(identity): implement AzureManagedIdentity.RunRefreshLoop

--- identity/azure.go
+++ identity/azure.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,19 +18,39 @@ var (
 	ErrInvalidEndpoint = errors.New("invalid endpoint")
 )
 
-type AzureIdentity struct {
+type AzureManagedIdentityOption func(*azureFetchOption)
+
+type azureFetchOption struct {
+	maxAttempts int
+}
+
+func WithMaxAttempts(n int) AzureManagedIdentityOption {
+	return func(o *azureFetchOption) {
+		o.maxAttempts = n
+	}
+}
+
+type AzureManagedIdentity struct {
 	AccessToken string
 	ExpiresOn   time.Time
 }
 
 // https://github.com/Azure/azure-sdk-for-go/blob/main/sdk/azidentity/TROUBLESHOOTING.md#verify-the-app-service-managed-identity-endpoint-is-available
-func GetAzureManagedIdentity(ctx context.Context) (*AzureIdentity, error) {
+func GetAzureManagedIdentity(ctx context.Context, opts ...AzureManagedIdentityOption) (*AzureManagedIdentity, error) {
 	const (
 		apiVersion          = "2019-08-01"
 		resource            = "https://ossrdbms-aad.database.windows.net" // also work with "https://management.core.windows.net/"
 		envIdentityEndpoint = "IDENTITY_ENDPOINT"
 		envIdentityHeader   = "IDENTITY_HEADER"
 	)
+
+	opt := azureFetchOption{
+		maxAttempts: 5,
+	}
+
+	for _, f := range opts {
+		f(&opt)
+	}
 
 	endpoint := os.Getenv(envIdentityEndpoint)
 	if endpoint == "" {
@@ -49,32 +69,31 @@ func GetAzureManagedIdentity(ctx context.Context) (*AzureIdentity, error) {
 
 	u.RawQuery = q.Encode()
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("invalid request: %w", err)
 	}
 
 	req.Header.Add("x-identity-header", os.Getenv(envIdentityHeader))
 
 	var (
 		res         *http.Response
-		maxAttempts = 5
+		maxAttempts = opt.maxAttempts
 	)
 
-	//
 	for {
-		res, err = http.DefaultClient.Do(req)
+		res, err = defaultFetcher.Fetch(ctx, req)
 		if err == nil {
 			break
 		}
 
 		maxAttempts--
 
-		if maxAttempts == 0 {
-			return nil, err
+		if maxAttempts <= 0 {
+			return nil, fmt.Errorf("filed to get: %w", err)
 		}
 
-		log.Println(err.Error(), "retrying in 5 seconds...")
+		slog.Warn("go-pkg/identity: retrying in 5 seconds...", "error", err)
 		time.Sleep(5 * time.Second)
 	}
 
@@ -125,8 +144,89 @@ func GetAzureManagedIdentity(ctx context.Context) (*AzureIdentity, error) {
 		return nil, fmt.Errorf("already expired: %s", t)
 	}
 
-	return &AzureIdentity{
+	return &AzureManagedIdentity{
 		AccessToken: st,
 		ExpiresOn:   t,
 	}, nil
+}
+
+func (a *AzureManagedIdentity) RunRefreshLoop(
+	ctx context.Context,
+	callback func(*AzureManagedIdentity, error) error,
+	opts ...AzureManagedIdentityOption,
+) error {
+	const retryInterval = 5 * time.Minute
+	d, err := refreshDuration(a.ExpiresOn)
+	if err != nil {
+		return err
+	}
+	ti := time.NewTimer(d)
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case <-ti.C:
+				var d time.Duration
+
+				token, err := GetAzureManagedIdentity(ctx, opts...)
+				if err == nil && token.AccessToken == a.AccessToken {
+					err = errors.New("token is not updated")
+				}
+
+				if err == nil {
+					d, err = refreshDuration(token.ExpiresOn)
+				}
+
+				if err != nil {
+					slog.Warn("go-pkg/identity: failed to get new azure managed identity", "error", err)
+				} else {
+					err = callback(token, nil)
+					if err != nil {
+						slog.Warn("go-pkg/identity: cannot use a new azure managed identity in callback", "error", err)
+					}
+				}
+
+				// retry
+				if err != nil {
+					if time.Until(a.ExpiresOn) < retryInterval {
+						_ = callback(nil, fmt.Errorf("token expired too soon, cannot refresh token: expires_on=%s", a.ExpiresOn))
+						return
+					}
+
+					ti.Reset(retryInterval)
+					break
+				}
+
+				a.AccessToken = token.AccessToken
+				a.ExpiresOn = token.ExpiresOn
+
+				ti.Reset(d)
+			}
+		}
+	}()
+
+	return nil
+}
+
+func refreshDuration(t time.Time) (time.Duration, error) {
+	const refreshMargion = 1 * time.Hour
+
+	d := time.Until(t)
+	if d < 0 {
+		return 0, fmt.Errorf("unexpected negative refresh duration (avoiding infinity loop):  expires_on=%s", t)
+	}
+
+	if d < refreshMargion {
+		slog.Warn("go-pkg/identity: duration is too short", "duration", d)
+		return d, nil
+	}
+
+	d -= refreshMargion
+
+	slog.Info("go-pkg/identity: refresh duration", "duration", d)
+
+	return d, nil
 }
